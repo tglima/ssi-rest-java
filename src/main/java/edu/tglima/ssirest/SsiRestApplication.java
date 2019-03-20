4e4aabc267fabed39494782e476745366c0dc368
Thiago Lima de Sousa <thg.limadesousa@gmail.com>
2019-03-20 08:23:27 -0300

    Cria a base do projeto

---
+++ src/main/java/edu/tglima/ssirest/SsiRestApplication.java
@@ -0,0 +1,13 @@
+package edu.tglima.ssirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsiRestApplication.class, args);
+	}
+
+}
